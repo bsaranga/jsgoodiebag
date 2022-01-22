136c2c0eddaf652b945f68212fed124e9c01d330
bsaranga <bsaranga@gmail.com>
2022-01-22 20:53:32 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import Prototypes from './prototypal-inheritance/prototypes';
 import './index.css';
 
-Prototypes();
+// The application is composed of a state object(s)
+// Read only state tree (immutable)
+// State changes are described by actions
+// Actions are 'dispatched'
+// All data changes are done via actions
+// Next state of the application is determined by the previous state and the action
+
+function counter(state, action) {
+  return state;
+}
