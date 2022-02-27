14ffdf0c8750412dad416a447fad7f33a2417b02
bsaranga <bsaranga@gmail.com>
2022-02-27 22:50:10 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-import './index.css';
+// Prototypes
+
+/*
+let person = {
+  name: 'Buwaneka Saranga',
+  age: 29,
+};
+
+let employee = {
+  position: 'Software Developer',
+  company: 'Zone24x7',
+};
+
+employee.__proto__ = person;
+
+console.log(employee.name); // name is missing from employee object, therefore JS goes into the prototype to get it
+console.log(employee.age); // similar behavior here
+*/
+
+let person = {
+  setName: function (name) {
+    this.name = name;
+  },
+};
+
+let saranga = {
+  __proto__: person,
+};
+
+saranga.setName('jdfhsk');
+console.log(saranga.name);
